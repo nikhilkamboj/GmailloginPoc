980715589a53b04192f3692f16eb9db0e1733003
nikhil <n.kamboj15@gmail.com>
2018-10-12 22:28:15 +0530

    gmail authentication added

---
+++ app/src/main/java/androidapp/com/gmailloginpoc/GmailLogin.java
@@ -0,0 +1,13 @@
+package androidapp.com.gmailloginpoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GmailLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gmail_login);
+    }
+}
